588d20906a31bddef9623d1a0534735cef076a2d
julileon10 <julio.leon.diaz1@gmail.com>
2022-01-18 15:54:37 -0500

    Update books.js

--- books.js
+++ books.js
@@ -4,6 +4,16 @@ const books = [
     title: 'Building API for Fun and Profit',
     author: 'Your Name Here',
   },
+  {
+    id: 0002,
+    title: 'Updating API for Fun and Profit',
+    author: 'Your First Name Here',
+  },
+  {
+    id: 0003,
+    title: 'Deleting API for Fun and Profit',
+    author: 'Your Last Name Here',
+  },
 ];
 
 module.exports = books;
